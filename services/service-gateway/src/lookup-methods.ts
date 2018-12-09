78b55c05a99cdac741ca2b9e8f6bec822dc64e43
Jean-Guillaume Buret <hello@jgburet.me>
2018-12-10 00:34:59 +0100

    feat(service-gateway): rdap task

--- services/service-gateway/src/lookup-methods.ts
+++ services/service-gateway/src/lookup-methods.ts
@@ -1,5 +1,5 @@
 const LOOKUP_METHODS = process.env.LOOKUP_METHODS
   ? process.env.LOOKUP_METHODS.split(",")
-  : ["echo", "geoIp", "whois"];
+  : ["echo", "geoIp", "rdap"];
 
 export default LOOKUP_METHODS;
